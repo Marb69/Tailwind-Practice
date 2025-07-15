c29f152896786c9e37ae7f1c11ba7216ac4d7c12
Jhon <jhonalvertmarbebe0@gmail.com>
2025-07-16 07:28:50 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // or adjust to your structure
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins:['Poppins ','sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}

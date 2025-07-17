fbc8303e57735b8e4be8b70d4f2ac22feeef4e9d
Jhon <jhonalvertmarbebe0@gmail.com>
2025-07-17 09:20:19 +0800

    "add some function"

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ let menu = document.querySelector('#menu');
 let span1 = document.querySelector('#span1');
 let span2 = document.querySelector('#span2');
 let span3 = document.querySelector('#span3');
-
+let overlay = document.querySelector('#overlay');
 
 function tog(){
 
@@ -13,13 +13,37 @@ function tog(){
     span1.classList.toggle('exit1');
      span2.classList.toggle('exit2');
  span3.classList.toggle('exit3');   
-    menu.addEventListener("click",function(){
+  overlay.classList.toggle('over');
+   document.body.classList.toggle('overflow-hidden');
+ 
+  
+}
+
+  menu.addEventListener("click",function(){
 
 
         menu.classList.toggle('hidden');
         menu.classList.toggle('flex');
+          menu.classList.toggle('open-menu');
+           span1.classList.toggle('exit1');
+     span2.classList.toggle('exit2');
+ span3.classList.toggle('exit3');   
+   overlay.classList.toggle('over');
+    document.body.classList.toggle('overflow-hidden');
     });
 
-}
+    
+  overlay.addEventListener("click",function(){
+
+
+        menu.classList.toggle('hidden');
+        menu.classList.toggle('flex');
+          menu.classList.toggle('open-menu');
+           span1.classList.toggle('exit1');
+     span2.classList.toggle('exit2');
+ span3.classList.toggle('exit3');   
+   overlay.classList.toggle('over');
+    document.body.classList.toggle('overflow-hidden');
+    });
 
 btnMenu.addEventListener('click',tog);
